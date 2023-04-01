75e736cc15fb74fa4ce6905ebdf3bb34c9d041fd
Sai Phanindra <saiphanindra265@gmail.com>
2023-04-01 16:29:05 +0530

    rendering multiple components

--- app.js
+++ app.js
@@ -21,34 +21,711 @@ import './index.css';
 //     <h1> Header Component </h1>
 // </div> )
 
+let restraunts = [
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "372537",
+      "name": "Madhuram tiffins and meals",
+      "uuid": "fa54a68f-5db3-4e18-9d88-1d9f9391a890",
+      "city": "75",
+      "area": "Hanamakonda",
+      "totalRatingsString": "500+ ratings",
+      "cloudinaryImageId": "nuzpxgu6pxr5nirmioiw",
+      "cuisines": [
+        "South Indian"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 10000,
+      "costForTwoString": "₹100 FOR TWO",
+      "deliveryTime": 27,
+      "minDeliveryTime": 27,
+      "maxDeliveryTime": 27,
+      "slaString": "27 MINS",
+      "lastMileTravel": 7.199999809265137,
+      "slugs": {
+        "restaurant": "madhuram-tiffins-and-meals-hanamkonda-hanamkonda",
+        "city": "warangal"
+      },
+      "cityState": "75",
+      "address": "2, Excise Colony Main Rd, KLN Reddy Colony, Subedari, Hanamkonda, Telangana 506001, India",
+      "locality": "Excise Colony Main Road",
+      "parentId": 128685,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "10% off",
+        "shortDescriptionList": [
+          {
+            "meta": "10% off | Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "10% off up to ₹40 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "10% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "10% off up to ₹40 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "ribbon": [
+        {
+          "type": "PROMOTED"
+        }
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 4200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 4200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "4200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=6280170~p=1~eid=00000187-30b4-bc11-04e8-c38900b00176",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "7.1 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "372537",
+        "deliveryTime": 27,
+        "minDeliveryTime": 27,
+        "maxDeliveryTime": 27,
+        "lastMileTravel": 7.199999809265137,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "4.1",
+      "totalRatings": 500,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "656568",
+      "name": "Srinivasa Tiffin Center",
+      "uuid": "bd285a4e-8cfa-4824-9074-7f3b7b831fb4",
+      "city": "75",
+      "area": "Shanthi Nagar",
+      "totalRatingsString": "Too Few Ratings",
+      "cloudinaryImageId": "6dea5c5e746b3ff5b9753404682974d0",
+      "cuisines": [
+        "South Indian"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 10000,
+      "costForTwoString": "₹100 FOR TWO",
+      "deliveryTime": 26,
+      "minDeliveryTime": 26,
+      "maxDeliveryTime": 26,
+      "slaString": "26 MINS",
+      "lastMileTravel": 0.30000001192092896,
+      "slugs": {
+        "restaurant": "srinivasa-tiffin-center-warangal-warangal",
+        "city": "warangal"
+      },
+      "cityState": "75",
+      "address": "16-1-87/1, Kareemabad Rd, Indira Nagar, Shanthi Nagar, Warangal, Telangana 506002, India",
+      "locality": "Kareemabad Road",
+      "parentId": 194948,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 1900,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 1900,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "1900",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "0.3 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "656568",
+        "deliveryTime": 26,
+        "minDeliveryTime": 26,
+        "maxDeliveryTime": 26,
+        "lastMileTravel": 0.30000001192092896,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "--",
+      "totalRatings": 0,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "110446",
+      "name": "Kailash Sweets",
+      "uuid": "c2053acc-3a9f-47e1-b380-58bbe21d4cd5",
+      "city": "75",
+      "area": "Shiva Nagar",
+      "totalRatingsString": "100+ ratings",
+      "cloudinaryImageId": "bf7tsm3hytcst7yi5jbc",
+      "cuisines": [
+        "Desserts"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 15000,
+      "costForTwoString": "₹150 FOR TWO",
+      "deliveryTime": 31,
+      "minDeliveryTime": 31,
+      "maxDeliveryTime": 31,
+      "slaString": "31 MINS",
+      "lastMileTravel": 0.800000011920929,
+      "slugs": {
+        "restaurant": "kailash-sweets-bomalla-gudi-warangal",
+        "city": "warangal"
+      },
+      "cityState": "75",
+      "address": "17-4-138,bomalla gudi, kareemabad",
+      "locality": "Bomalla Gudi",
+      "parentId": 113581,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 1900,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 1900,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "1900",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "0.8 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "110446",
+        "deliveryTime": 31,
+        "minDeliveryTime": 31,
+        "maxDeliveryTime": 31,
+        "lastMileTravel": 0.800000011920929,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "4.1",
+      "totalRatings": 100,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "99554",
+      "name": "New Maruthi tiffins",
+      "uuid": "575955bc-315d-4b7c-ae0f-76becce8b5cb",
+      "city": "75",
+      "area": "Hanamakonda",
+      "totalRatingsString": "5000+ ratings",
+      "cloudinaryImageId": "hxoz63mldxl4iijoe7ua",
+      "cuisines": [
+        "South Indian"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 10000,
+      "costForTwoString": "₹100 FOR TWO",
+      "deliveryTime": 24,
+      "minDeliveryTime": 24,
+      "maxDeliveryTime": 24,
+      "slaString": "24 MINS",
+      "lastMileTravel": 6.900000095367432,
+      "slugs": {
+        "restaurant": "new-maruthi-tiffins-nakkalagutta-hanamkonda-hanamkonda",
+        "city": "warangal"
+      },
+      "cityState": "75",
+      "address": "Circuit house road,hanamkonda",
+      "locality": "Circuit House Road",
+      "parentId": 147252,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "10% off",
+        "shortDescriptionList": [
+          {
+            "meta": "10% off | Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "10% off up to ₹40 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "10% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "10% off up to ₹40 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "ribbon": [
+        {
+          "type": "PROMOTED"
+        }
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 4200,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 4200,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "4200",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=6280162~p=4~eid=00000187-30b4-bc11-04e8-c38a00b00464",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "6.9 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "99554",
+        "deliveryTime": 24,
+        "minDeliveryTime": 24,
+        "maxDeliveryTime": 24,
+        "lastMileTravel": 6.900000095367432,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "3.9",
+      "totalRatings": 5000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "484503",
+      "name": "Srikrishna Bavan Hotel",
+      "uuid": "cb66ccd7-07f1-4c7d-aead-a619024fc6af",
+      "city": "75",
+      "area": "Girmajipet",
+      "totalRatingsString": "100+ ratings",
+      "cloudinaryImageId": "rejs01yszmhhscfua7yf",
+      "cuisines": [
+        "South Indian",
+        "Indian"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 11000,
+      "costForTwoString": "₹110 FOR TWO",
+      "deliveryTime": 22,
+      "minDeliveryTime": 22,
+      "maxDeliveryTime": 22,
+      "slaString": "22 MINS",
+      "lastMileTravel": 1.7999999523162842,
+      "slugs": {
+        "restaurant": "srikrishna-bavan-hotel-warangal-warangal",
+        "city": "warangal"
+      },
+      "cityState": "75",
+      "address": "9-10-44, Durgeshwara Swamy Temple line, Gurmajipet line, warangal, Warangal (Town), Warangal (Urban), Telangana - 506002",
+      "locality": "Powers Colony",
+      "parentId": 290856,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "20% off",
+        "shortDescriptionList": [
+          {
+            "meta": "20% off | Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "20% off up to ₹50 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "20% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "20% off up to ₹50 | Use code TRYNEW",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          {
+            "name": "distance",
+            "fee": 1900,
+            "message": ""
+          },
+          {
+            "name": "time",
+            "fee": 0,
+            "message": ""
+          },
+          {
+            "name": "special",
+            "fee": 0,
+            "message": ""
+          }
+        ],
+        "totalFees": 1900,
+        "message": "",
+        "title": "Delivery Charge",
+        "amount": "1900",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "1.7 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "484503",
+        "deliveryTime": 22,
+        "minDeliveryTime": 22,
+        "maxDeliveryTime": 22,
+        "lastMileTravel": 1.7999999523162842,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "3.7",
+      "totalRatings": 100,
+      "new": false
+    },
+    "subtype": "basic"
+  }
+  ]
+
 const Body = () =>{
     return(
         <div className="bodyContainer">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestroCard />
-                <RestroCard />
-                <RestroCard />
-                <RestroCard />
-                <RestroCard />
-                <RestroCard />
-                <RestroCard />
+                {
+                    restraunts.map(res =>  <RestroCard resData={res} /> )
+                }
             </div>
         </div>
     )
 }
 
-const RestroCard = () => {
+const RestroCard = ({resData}) => {
+    const {name, cuisines, cloudinaryImageId, costForTwo, deliveryTime } = resData.data
+    const baseUrl = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
     return(
         <div className="res-card">
             <div className="res-img">
-                <img src="https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/u2aycylttqjfrcs4clzb" />
+                <img src={baseUrl+cloudinaryImageId} />
             </div>
             <div className="res-details">
-                <h3>Paradise</h3>
-                <h4>Nakkalagutta, Hanamkonda</h4>
-                <h4>4.0</h4>
-                <h4>30 mins</h4>
+                <h3>{name}</h3>
+                <h4>{cuisines.join(', ')}</h4>
+                <h4>{costForTwo/100} for two</h4>
+                <h4>Delivers in {deliveryTime} mins</h4>
             </div>
         </div>
     )
